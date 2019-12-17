e72d4b44c8cce5b45815a0eae3582572702186e8
ExploShot <shotexplo@gmail.com>
2019-12-17 19:58:31 +0100

    Revert "Fix Codestyle issues in /src/CryptoNoteProtocol"
    This reverts commit 948234825e1b8b8d5b9e6003cf617061db5b1a3e.

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandlerCommon.h
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandlerCommon.h
@@ -25,17 +25,16 @@
 
 namespace CryptoNote
 {
-    struct NOTIFY_NEW_BLOCK_request;
+  struct NOTIFY_NEW_BLOCK_request;
 
-    struct ICryptoNoteProtocol 
-    {
-        virtual void relayBlock(NOTIFY_NEW_BLOCK_request &arg) = 0;
-        virtual void relayTransactions(const std::vector<BinaryArray> &transactions) = 0;
-    };
-    
-    struct ICryptoNoteProtocolHandler 
-        : ICryptoNoteProtocol, 
-          public ICryptoNoteProtocolQuery 
-    {
-    };
+  /************************************************************************/
+  /*                                                                      */
+  /************************************************************************/
+  struct ICryptoNoteProtocol {
+    virtual void relayBlock(NOTIFY_NEW_BLOCK_request& arg) = 0;
+    virtual void relayTransactions(const std::vector<BinaryArray>& transactions) = 0;
+  };
+  
+  struct ICryptoNoteProtocolHandler : ICryptoNoteProtocol, public ICryptoNoteProtocolQuery {
+  };
 }
