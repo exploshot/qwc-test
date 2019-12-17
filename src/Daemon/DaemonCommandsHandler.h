cdcb4d842a3bef6238f33cc9b94c974c3795ba16
ExploShot <shotexplo@gmail.com>
2019-12-17 23:10:24 +0100

    Fix Codestyle issues in /src/Daemon

--- src/Daemon/DaemonCommandsHandler.h
+++ src/Daemon/DaemonCommandsHandler.h
@@ -16,61 +16,65 @@
 #include <Rpc/JsonRpc.h>
 
 namespace CryptoNote {
-class Core;
-class NodeServer;
-}
+    class Core;
+    class NodeServer;
+} // namespace CryptoNote
 
 class DaemonCommandsHandler
 {
 public:
-  DaemonCommandsHandler(CryptoNote::Core& core, CryptoNote::NodeServer& srv, std::shared_ptr<Logging::LoggerManager> log, CryptoNote::RpcServer* prpc_server);
+    DaemonCommandsHandler(CryptoNote::Core &core,
+                          CryptoNote::NodeServer &srv,
+                          std::shared_ptr<Logging::LoggerManager> log,
+                          CryptoNote::RpcServer *prpc_server);
 
-  bool start_handling() {
-    m_consoleHandler.start();
-    return true;
-  }
+    bool start_handling()
+    {
+        m_consoleHandler.start ();
+        return true;
+    }
 
-  void stop_handling() {
-    m_consoleHandler.stop();
-  }
+    void stop_handling()
+    {
+        m_consoleHandler.stop ();
+    }
 
-  bool exit(const std::vector<std::string>& args);
+    bool exit(const std::vector<std::string> &args);
 
 private:
 
-  Common::ConsoleHandler m_consoleHandler;
-  CryptoNote::Core& m_core;
-  CryptoNote::NodeServer& m_srv;
-  Logging::LoggerRef logger;
-  std::shared_ptr<Logging::LoggerManager> m_logManager;
-  CryptoNote::RpcServer* m_prpc_server;
+    Common::ConsoleHandler m_consoleHandler;
+    CryptoNote::Core &m_core;
+    CryptoNote::NodeServer &m_srv;
+    Logging::LoggerRef logger;
+    std::shared_ptr<Logging::LoggerManager> m_logManager;
+    CryptoNote::RpcServer *m_prpc_server;
 
-  std::string get_commands_str();
-  bool print_block_by_height(uint32_t height);
-  bool print_block_by_hash(const std::string& arg);
+    std::string get_commands_str();
+    bool print_block_by_height(uint32_t height);
+    bool print_block_by_hash(const std::string &arg);
 
-  bool help(const std::vector<std::string>& args);
-  bool print_pl(const std::vector<std::string>& args);
-  bool show_hr(const std::vector<std::string>& args);
-  bool hide_hr(const std::vector<std::string>& args);
-  bool print_bc_outs(const std::vector<std::string>& args);
-  bool print_cn(const std::vector<std::string>& args);
-  bool print_bc(const std::vector<std::string>& args);
-  bool print_bci(const std::vector<std::string>& args);
-  bool set_log(const std::vector<std::string>& args);
-  bool print_block(const std::vector<std::string>& args);
-  bool print_tx(const std::vector<std::string>& args);
-  bool print_pool(const std::vector<std::string>& args);
-  bool print_pool_sh(const std::vector<std::string>& args);
-  bool start_mining(const std::vector<std::string>& args);
-  bool stop_mining(const std::vector<std::string>& args);
-  bool status(const std::vector<std::string>& args);
-  // ip banning
-  bool ip_ban(const std::vector<std::string>& args);
-  bool ip_unban(const std::vector<std::string>& args);
-  bool ip_unban_all(const std::vector<std::string>& args);
+    bool help(const std::vector<std::string> &args);
+    bool print_pl(const std::vector<std::string> &args);
+    bool show_hr(const std::vector<std::string> &args);
+    bool hide_hr(const std::vector<std::string> &args);
+    bool print_bc_outs(const std::vector<std::string> &args);
+    bool print_cn(const std::vector<std::string> &args);
+    bool print_bc(const std::vector<std::string> &args);
+    bool print_bci(const std::vector<std::string> &args);
+    bool set_log(const std::vector<std::string> &args);
+    bool print_block(const std::vector<std::string> &args);
+    bool print_tx(const std::vector<std::string> &args);
+    bool print_pool(const std::vector<std::string> &args);
+    bool print_pool_sh(const std::vector<std::string> &args);
+    bool start_mining(const std::vector<std::string> &args);
+    bool stop_mining(const std::vector<std::string> &args);
+    bool status(const std::vector<std::string> &args);
 
-  //// tx threshold
-  //bool tx_threshold(const std::vector<std::string>& args);
-  //bool tx_threshold_set(const std::vector<std::string>& args);
+    /*!
+     * ip banning
+     */
+    bool ip_ban(const std::vector<std::string> &args);
+    bool ip_unban(const std::vector<std::string> &args);
+    bool ip_unban_all(const std::vector<std::string> &args);
 };
