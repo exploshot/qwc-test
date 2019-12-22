857e55eb1910baeb95a2dee9cd139d7478a8f965
ExploShot <shotexplo@gmail.com>
2019-12-22 03:34:07 +0100

    Moved src/WalletBackend/Constants to src/Global/Constants

--- src/Global/CryptoNoteConfig.h
+++ src/Global/CryptoNoteConfig.h
@@ -220,7 +220,7 @@ namespace CryptoNote {
 		 */
 		const uint8_t CURRENT_FORK_INDEX                                    = FORK_HEIGHTS_SIZE == 0 ? 0 : SOFTWARE_SUPPORTED_FORK_INDEX;
 
-		static_assert(CURRENT_FORK_INDEX >= 0, "CURRENT FORK INDEX must be >= 0");
+		// static_assert(CURRENT_FORK_INDEX >= 0, "CURRENT FORK INDEX must be >= 0");
 		/*!
 		 * Make sure CURRENT_FORK_INDEX is a valid index, unless FORK_HEIGHTS is empty
 		 */
